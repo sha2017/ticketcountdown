b01fa8435aa183975e5b4e0793cdd3512604eef0
Elger Jonker <elger.jonker@gmail.com>
2017-06-19 16:50:03 +0200

    It works on my box :D
    UNDOCUMENTATION

---
+++ countdown.py
@@ -0,0 +1,62 @@
+# pip3 install mysqlclient
+import MySQLdb
+import http.server
+
+DATABASE_HOST = 'localhost'
+DATABASE_USER = 'pretix'
+DATABASE_PASS = ''
+DATABASE_DB = 'pretix'
+
+SERVER_IP = '0.0.0.0'
+SERVER_PORT = 4242
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_HEAD(self):
+        return self.do_GET()
+
+    def do_GET(self):
+
+        if self.path != "/index.json":
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Ticket counter sponsored by E Corp. Try index.json.\n".encode("utf-8"))
+            return
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(self.numbers().encode("utf-8"))
+
+    def numbers(self):
+        db = MySQLdb.connect(host=DATABASE_HOST,
+                             user=DATABASE_USER,
+                             passwd=DATABASE_PASS,
+                             db=DATABASE_DB)
+        cur = db.cursor()
+        cur.execute("""select 
+                count(pretixbase_orderposition.item_id) as count,
+                max(pretixbase_order.datetime) as latest
+            FROM
+                pretixbase_order
+            INNER JOIN 
+                pretixbase_orderposition on pretixbase_orderposition.order_id = pretixbase_order.id
+            INNER JOIN
+                pretixbase_item ON pretixbase_orderposition.item_id = pretixbase_item.id
+            WHERE
+                pretixbase_item.admission=1
+            AND
+                pretixbase_order.status IN ('p','n')""")
+
+        answer = ""
+        for row in cur.fetchall():
+            answer = ("{\"ordered\":%d, \"last_order\":'%s', \"tickets_left\":%d}\n" %
+                      (row[0], row[1], 3000 - int(row[0])))
+
+        db.close()
+
+        return answer
+
+server = http.server.HTTPServer((SERVER_IP, SERVER_PORT), MyHandler)
+server.serve_forever()
